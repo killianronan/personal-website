079d0fd45690c7bcb82816d8dfbcf1e0b3256c0c
Killian <kronan@tcd.ie>
2024-06-21 19:45:05 -0700

    responsive header & featured projects added to home page

--- personal-website/src/components/header/Header.js
+++ personal-website/src/components/header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaHome, FaUser, FaProjectDiagram } from "react-icons/fa";
 import "./Header.css";
 
 function Header() {
@@ -10,12 +11,21 @@ function Header() {
         <p className="subtitle">Software Development</p>
       </div>
       <nav className="nav">
-        <Link className="link" to="/">Home</Link>
-        <Link className="link" to="/about">About</Link>
-        <Link className="link" to="/projects">Projects</Link>
+        <Link className="link" to="/">
+          <span className="nav-text">Home</span>
+          <FaHome className="nav-icon" />
+        </Link>
+        <Link className="link" to="/about">
+          <span className="nav-text">About</span>
+          <FaUser className="nav-icon" />
+        </Link>
+        <Link className="link" to="/projects">
+          <span className="nav-text">Projects</span>
+          <FaProjectDiagram className="nav-icon" />
+        </Link>
       </nav>
       <div className="name-role">
-        <button className="resume-button" onClick={() => {window.open("https://drive.google.com/file/d/1XGKmkF2tqkeZXGGggs1U_Z1eB9_8qTdB/view");}}>Resume</button>
+        <button className="resume-button" onClick={() => window.open("https://drive.google.com/file/d/1XGKmkF2tqkeZXGGggs1U_Z1eB9_8qTdB/view")}>Resume</button>
       </div>
     </header>
   );
--- personal-website/src/components/home/Home.js
+++ personal-website/src/components/home/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
-import "./Home.css";
 import Typewriter from "typewriter-effect";
+import "./Home.css";
+import { SiStarship } from "react-icons/si";
+import { IoIosFootball } from "react-icons/io";
+
+
+const featuredProjects = [
+  {
+    name: 'Extraterrestrial Tracker App',
+    url: 'https://github.com/killianronan/extraterrestrial-celestial-tracker',
+    description: 'Built a web application designed to predict and notify users about upcoming celestial events.',
+    icon: <SiStarship className="project-icon" />
+  },
+  {
+    name: 'Football Match Predictor',
+    url: 'https://github.com/killianronan/Premier-League-ML-Analysis',
+    description: 'Created a football match outcome prediction system using various machine learning models in python.',
+    icon: <IoIosFootball className="project-icon" />
+  }
+];
 
 function Home() {
   return (
@@ -29,6 +47,7 @@ function Home() {
             <FaGithub
               className="icon github"
               aria-label="GitHub"
+              title="GitHub"
               onClick={() => {
                 window.open("https://github.com/killianronan/");
               }}
@@ -36,6 +55,7 @@ function Home() {
             <FaLinkedin
               className="icon linkedin"
               aria-label="LinkedIn"
+              title="LinkedIn"
               onClick={() => {
                 window.open("https://www.linkedin.com/in/killian-ronan-9b5150194/");
               }}
@@ -43,6 +63,7 @@ function Home() {
             <FaInstagram
               className="icon insta"
               aria-label="Instagram"
+              title="Instagram"
               onClick={() => {
                 window.open("https://instagram.com/killianronan99");
               }}
@@ -50,7 +71,26 @@ function Home() {
           </nav>
         </header>
         <aside className="image-container">
-          <div className="development-image"></div>
+          <h2 className="featured-title">Featured Projects</h2>
+          <div className="featured-projects-container">
+            {featuredProjects.map((project) => (
+              <div className="project-card" key={project.name}>
+                <div className="project-icon-container">
+                  {project.icon}
+                </div>
+                <div className="project-content">
+                  <h3 className="project-title">{project.name}</h3>
+                  <p className="project-description">{project.description}</p>
+                </div>
+                <div className="project-links">
+                  <a href={project.url} target="_blank" rel="noopener noreferrer" className="github-link">
+                    <FaGithub className="project-repo-icon" />
+                    <span className="tooltip-text">Open GitHub</span>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
         </aside>
       </div>
     </section>
